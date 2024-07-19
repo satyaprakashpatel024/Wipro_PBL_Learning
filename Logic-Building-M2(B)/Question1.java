c564217eedffc9af55bf5864df6c5f08298bf94e
satyaprakashpatel024 <satyaprakashpatel61@gmail.com>
2024-07-19 16:51:42 +0530

    Question one solved

--- Logic-Building-M2(B)/Question1.java
+++ Logic-Building-M2(B)/Question1.java
@@ -1,5 +1,17 @@
 public class Question1{
+    public static int generateSeries(int a,int b, int c,int n){
+        int []arr = new int[2];
+        arr[0] = b-a;
+        arr[1] = c-b;
+        int strt = a;
+        for(int i=0; i<n-1; i++){
+            System.out.print(strt+",");
+            strt += arr[i%2];
+        }
+        return strt;
+    }
     public static void main(String[] args) {
-        
+        int ans = generateSeries(1,3,6,17);
+        System.out.println(ans);
     }
 }
